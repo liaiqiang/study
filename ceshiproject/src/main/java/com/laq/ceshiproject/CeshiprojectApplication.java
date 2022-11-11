df44295794802c57938fc71b8a81c64dd3b62e53
liaiqiang <18612124689@163.com>
2022-11-11 11:44:20 +0800

    第一次

---
+++ ceshiproject/src/main/java/com/laq/ceshiproject/CeshiprojectApplication.java
@@ -0,0 +1,13 @@
+package com.laq.ceshiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeshiprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CeshiprojectApplication.class, args);
+    }
+
+}
---
+++ ceshiproject/src/test/java/com/laq/ceshiproject/CeshiprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laq.ceshiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeshiprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
